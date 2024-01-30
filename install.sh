c14c3d51dadc4d2523c79bb5b1571c7c707a26d7
Laszlo Merczel <laszlo.merczel@gmail.com>
2024-01-30 14:57:07 +0000

    Minor fixes made to install.sh

--- install.sh
+++ install.sh
@@ -3,17 +3,15 @@
 function install_for_debian() {
 
   apt update
-  apt upgrade
+  apt upgrade -y
   apt install -y gpg wget
 
   wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
   echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
   apt update
-  apt install packer
+  apt install -y packer lxc
 
-  packer version
-  packer init
   packer plugins install github.com/hashicorp/lxc
 }
 
@@ -25,12 +23,18 @@ function install_for_alpine() {
   echo Alpine is not yet supported.
 }
 
-OS_GUESS=$(which yum >/dev/null 2>&1 && echo CentOS \
-        || which apt >/dev/null 2>&1 && echo Debian \
-        || which apk >/dev/null 2>&1 && echo Alpine \
-        || echo Unknown)
+OS_GUESS="Unknown"
+if [ ! -z $(which yum) ]; then
+  OS_GUESS="CentOS"
+elif [ ! -z $(which apt) ]; then
+  OS_GUESS="Debian"
+elif [ ! -z $(which apk) ]; then
+  OS_GUESS="Alpine"
+fi
 
-case $OS_GUESS in
+echo "Detected OS '$OS_GUESS'"
+
+case "$OS_GUESS" in
   CentOS)
     install_for_centos
     exit
