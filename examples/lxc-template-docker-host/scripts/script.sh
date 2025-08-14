2640df97be270f0a6486b119e33d701367545886
TheTinkerDad <laszlo.merczel@gmail.com>
2025-08-14 10:15:49 +0200

    Cleanup, fix non-interactve install, remove redundant sudo's

--- examples/lxc-template-docker-host/scripts/script.sh
+++ examples/lxc-template-docker-host/scripts/script.sh
@@ -5,13 +5,13 @@ sleep 30
 
 # OS Basics
 apt update
-apt upgrade
+apt upgrade -y
 
 # Clean up any leftover Docker packages from the OS default (if there's any)
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done
 
 # Add Docker's official GPG key
-apt-get install ca-certificates curl
+apt-get install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -20,11 +20,11 @@ chmod a+r /etc/apt/keyrings/docker.asc
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
 
 # Install the latest version of Docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Verify our installation
-sudo docker run hello-world
+docker run hello-world
