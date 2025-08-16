77e711f1ce50b36de817daa81ccb6abf20310a48
TheTinkerDad <laszlo.merczel@gmail.com>
2025-08-16 12:15:29 +0200

    Added another example: SteamCMD

---
+++ examples/lxc-template-steamcmd-server/scripts/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Let us wait a bit for the container's network to come alive!
+sleep 30
+
+# OS Basics & repo setup
+# Note: SteamCMD requires enabling 32bit arch
+#apt-add-repository multiverse
+dpkg --add-architecture i386
+apt update
+apt upgrade -y
+
+echo steam steam/question select "I AGREE" | debconf-set-selections
+echo steam steam/license note '' | debconf-set-selections
+
+# Package installation
+apt install -y software-properties-common steamcmd
+
